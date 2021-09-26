200f2a00531d23dea432aa4658fc2d27da67a49e
Benji Altman <benji@altmansoftwaredesign.com>
2021-09-26 01:22:59 -0500

    init

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ConsoleApp1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			foreach (var p in Process.GetProcesses()) Console.WriteLine(p.ProcessName);
+
+		}
+	}
+}
---
+++ InfiniteBeeps/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InfiniteBeeps
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string[] processesToKill = new string[]
+			{
+				"vlc"
+			};
+
+			foreach (var p in Process.GetProcesses())
+				if (processesToKill.Contains(p.ProcessName.Trim().ToLower()))
+					p.Kill();
+
+			Application.SetSuspendState(PowerState.Suspend, true, false);
+		}
+	}
+}
